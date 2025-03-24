b545cbf645b4e6dae1a950d89389d0d02355f8c4
Ognjen Djuricic <odjuricic@tenstorrent.com>
2025-03-24 15:00:16 +0100

    Shard 40% resnset

--- lib/Conversion/TTIRToTTNN/TTIRToTTNN.cpp
+++ lib/Conversion/TTIRToTTNN/TTIRToTTNN.cpp
@@ -446,7 +446,7 @@ public:
 
     auto reshapedGrad = mlir::tt::ttir_to_ttnn::utils::generateReshape(
         mlir::cast<TypedValue<mlir::RankedTensorType>>(adaptor.getInGradient()),
-        reshapedGradShape, rewriter);
+        reshapedGradShape, rewriter, "_reshaped_grad");
 
     // Get TTNNLayoutAttr of the result type.
     ttnn::TTNNLayoutAttr layoutAttr = mlir::cast<ttnn::TTNNLayoutAttr>(
@@ -1031,7 +1031,7 @@ public:
 
     Value flattenedInput = ttir_to_ttnn::utils::generateNHWFlatten(
         mlir::cast<mlir::TypedValue<RankedTensorType>>(adaptor.getInput()),
-        rewriter);
+        rewriter, "_flatten");
 
     // Convolution in ttnn returns a tensor in a flattened shape
     // (1 x 1 x N * H * W x C)
@@ -1051,8 +1051,8 @@ public:
         batchSizeAttr, inputHeightAttr, inputWidthAttr, kernelSizeAttr,
         *strideAttr, reducedPaddingAttr, *dilationAttr, groupsAttr, nullptr);
 
-    Value output =
-        ttir_to_ttnn::utils::generateReshape(newConv, outputShape, rewriter);
+    Value output = ttir_to_ttnn::utils::generateReshape(newConv, outputShape,
+                                                        rewriter, "_unflatten");
 
     rewriter.replaceOp(op, output);
     return success();
@@ -1237,7 +1237,7 @@ public:
 
     Value flattenedInput = ttir_to_ttnn::utils::generateNHWFlatten(
         mlir::cast<mlir::TypedValue<RankedTensorType>>(adaptor.getInput()),
-        rewriter);
+        rewriter, "_flatten");
 
     auto outputType = op.getResult().getType();
     llvm::ArrayRef<std::int64_t> outputShape = outputType.getShape();
@@ -1271,8 +1271,8 @@ public:
         kernelSizeAttr, strideAttr, paddingAttr, dilationAttr,
         adaptor.getCeilMode());
 
-    Value output =
-        ttir_to_ttnn::utils::generateReshape(newPool, outputShape, rewriter);
+    Value output = ttir_to_ttnn::utils::generateReshape(newPool, outputShape,
+                                                        rewriter, "_unflatten");
 
     rewriter.replaceOp(op, output);
 
