af16d05e6ee989bd72bcf77b9173ce67d1795daa
Collin Tod <collintod@tenstorrent.com>
2025-03-25 17:55:56 +0000

    fix rebase stuff

--- python/test_infra/ttir_builder.py
+++ python/test_infra/ttir_builder.py
@@ -904,9 +904,9 @@ class TTIRBuilder:
         weight: Operand,
         bias: Optional[Operand],
         in1: Operand,
-        stride: Union[IntegerAttr, DenseI32ArrayAttr],
-        padding: Union[IntegerAttr, DenseI32ArrayAttr],
-        dilation: Union[IntegerAttr, DenseI32ArrayAttr],
+        stride: Union[int, List[int]],
+        padding: Union[int, List[int]],
+        dilation: Union[int, List[int]],
         groups: int,
     ) -> OpView:
         if not bias:
@@ -970,10 +970,10 @@ class TTIRBuilder:
         weight: Operand,
         bias: Optional[Operand],
         in1: Operand,
-        stride: Union[IntegerAttr, DenseI32ArrayAttr],
-        padding: Union[IntegerAttr, DenseI32ArrayAttr],
-        output_padding: Union[IntegerAttr, DenseI32ArrayAttr],
-        dilation: Union[IntegerAttr, DenseI32ArrayAttr],
+        stride: Union[int, List[int]],
+        padding: Union[int, List[int]],
+        output_padding: Union[int, List[int]],
+        dilation: Union[int, List[int]],
         groups: int,
     ) -> OpView:
         if not bias:
